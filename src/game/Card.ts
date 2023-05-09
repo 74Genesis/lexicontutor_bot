6a6c2fed79a0e512de76db77f8394ef6f30844f5
Razumov Igor <74genesis@gmail.com>
2023-05-09 13:13:49 +0400

    game classes

--- src/game/Card.ts
+++ src/game/Card.ts
@@ -1,7 +1,10 @@
 export default class Card {
   public front: string = '';
   public back: string = '';
-  constructor(options: Record<string, any>) {}
+  constructor(options: Record<string, any>) {
+    this.front = options?.front;
+    this.back = options?.back;
+  }
 
   easy() {}
   medium() {}
